258d9b37e33b2a517afa342b16eea972ec3f9f99
joegit83 <73511087+joegit83@users.noreply.github.com>
2026-05-16 05:05:40 +0300

    update mobile width

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,27 +7,23 @@ import Logo3 from '../../assets/logo3.png';
 import { Link } from 'react-scroll';
 
 const Footer = () => {
-  return (
-    <div className="Footer-container">
-        
+    return (
+
         <div className="footer">
             <div className="social-links">
                 <Link class="link" to="https://www.instagram.com/p/DC7u7VZo3ab/">
-                <img src={Instagram}  alt=""/>
+                    <img src={Instagram} alt="" />
                 </Link>
                 <p>Wadi El Zayni-Saida</p>
                 <p>+961 71-342-627</p>
             </div>
             <div className="logo-f">
-                <img src={Logo3} alt=""/>
+                <img src={Logo3} alt="" />
                 <h1>Hani Afifi Fitness</h1>
 
             </div>
         </div>
-        <div className="blur blur-f-1"></div>
-        <div className="blur blur-f-2"></div>
-    </div>
-  )
+    )
 }
 
 export default Footer
